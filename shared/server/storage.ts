6a5604dbde6a47eeac5ed464e4b43eab70c13061
Justinsuen0917 <132532622+Justinsuen0917@users.noreply.github.com>
2025-02-19 21:02:44 -0800

    Create storage.ts

---
+++ shared/server/storage.ts
@@ -0,0 +1,39 @@
+import { keyboardThemes, type KeyboardTheme, type InsertKeyboardTheme } from "@shared/schema";
+
+export interface IStorage {
+  getThemes(): Promise<KeyboardTheme[]>;
+  getTheme(id: number): Promise<KeyboardTheme | undefined>;
+  createTheme(theme: InsertKeyboardTheme): Promise<KeyboardTheme>;
+  deleteTheme(id: number): Promise<void>;
+}
+
+export class MemStorage implements IStorage {
+  private themes: Map<number, KeyboardTheme>;
+  private currentId: number;
+
+  constructor() {
+    this.themes = new Map();
+    this.currentId = 1;
+  }
+
+  async getThemes(): Promise<KeyboardTheme[]> {
+    return Array.from(this.themes.values());
+  }
+
+  async getTheme(id: number): Promise<KeyboardTheme | undefined> {
+    return this.themes.get(id);
+  }
+
+  async createTheme(insertTheme: InsertKeyboardTheme): Promise<KeyboardTheme> {
+    const id = this.currentId++;
+    const theme: KeyboardTheme = { ...insertTheme, id };
+    this.themes.set(id, theme);
+    return theme;
+  }
+
+  async deleteTheme(id: number): Promise<void> {
+    this.themes.delete(id);
+  }
+}
+
+export const storage = new MemStorage();
